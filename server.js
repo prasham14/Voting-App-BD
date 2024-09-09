31ce07a2419dabd8dde3d4067f6c3988794edde0
= <prasham.22bcon114@jecrcu.edu.in>
2024-09-09 14:23:13 +0530

    Final Changes

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const candidateRoutes = require("./routes/candidateRoutes");
 app.use("/user", userRoutes);
 app.use("/candidate", candidateRoutes);
 app.get("/", (req, res) => {
-  res.send("Welcome to the Hotel");
+  res.send("Hello User , this Website do not contain any UI , this is a backend project ,please use a API testing tool for using this Project . Thank You ! ");
 });
 app.listen(PORT, () => {
   console.log("listening on port 3000");
